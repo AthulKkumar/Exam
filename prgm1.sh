823f1132dd7067275e60d73426962c18a32d5f15
Deepakajithmm <mailofdeepakajith@gmail.com>
2022-05-05 15:53:57 +0530

    first program is finished

---
+++ prgm1.sh
@@ -0,0 +1,16 @@
+echo "enter an number"
+read n
+b=$n
+s=0
+while [ $n -gt 0 ]
+do
+r=`expr $n % 10`
+s=`expr $s + $r \* $r \* $r`
+n=`expr $n / 10`
+done
+if [ $s -eq $b ]
+then
+echo "it is amstrong"
+else
+echo "it is not amstrong"
+fi
